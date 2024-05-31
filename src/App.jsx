e02fd2f2cc289000ff013546af4397af53661b8a
Square <aayushk4980@gmail.com>
2024-05-31 17:20:23 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import Header from './components/Header';
 import './style.css';
 
 function App() {
 
+    // Theme state.
+    const [theme, setTheme] = useState('light');
+
+    // This function toggle the theme state between light and dark.
+    const changeTheme = () => {
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
   return (
     <>
-      Dictionary App
+      <Header theme={theme} changeTheme={changeTheme} />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import photo from '../images/photo.jpeg';
+import iconSun from '../images/icon-sun.svg';
+import iconMoon from '../images/icon-moon.svg';
+
+export default function Header({theme, changeTheme}) {
+
+    // If theme is light, then show moon icon, else show sun icon.
+    const themeBtnIcon = theme === 'light' ? iconMoon : iconSun;
+
+    return (
+        <header className={`header flex ${theme}`}>
+            <h1>Dictionary</h1>
+            <div className='flex'>
+                <button className='changeThemeBtn' onClick={() => changeTheme()}>
+                    <img src={themeBtnIcon} alt="Change theme" />
+                </button>
+                <img src={photo} alt="Author" height={40} />
+            </div>
+        </header>
+    );
+}
