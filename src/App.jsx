175e31d30b89ba264d02768c811f40a272640396
Square <aayushk4980@gmail.com>
2024-06-03 00:30:47 +0530

    Local storage for theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import './style.css';
 import MainSection from './components/MainSection';
 
 function App() {
 
-    // Theme state.
-    const [theme, setTheme] = useState('dark');
+  // Getting initialUserData from local storage. If it is not present then adding default data.
+  const initialUserData = JSON.parse(localStorage.getItem('userData')) || ['dark', []];
 
-    // This function toggle the theme state between light and dark.
-    const changeTheme = () => {
-      setTheme(theme === 'light' ? 'dark' : 'light');
-    }
+  // Initializing a state which will help to update the local storage for userData.
+  let [userData, setUserData] = useState(initialUserData);
+
+  // Theme state.
+  const [theme, setTheme] = useState(userData[0]);
+
+  // Updates the localStorage when the userData updates.
+  useEffect(() => {
+    localStorage.setItem('userData', JSON.stringify(userData));
+  }, [userData]);
+
+  // Updates theme in userData when it is changed.
+  useEffect(() => {
+    setUserData([theme, ...userData.slice(1)]);
+  }, [theme]);
+
+  // This function toggle the theme state between light and dark.
+  const changeTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
 
   return (
     <>
